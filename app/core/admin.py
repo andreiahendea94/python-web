6b0cd8a65bfbebab6c5bfd11a0ecb1467d66171d
Andreia <andreia@mariadb.org>
2023-03-13 17:41:56 +0200

    Override User table and User authentication

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import User
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin"s
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    # add_form = UserCreationForm
+    list_display = UserAdmin.list_display + ("city", "country",
+                                             "phone_number", "date_of_birth")
+
+    for i in range(len(UserAdmin.fieldsets)):
+        if UserAdmin.fieldsets[i][0] == "Personal info":
+            UserAdmin.fieldsets[i][1]["fields"] += ("city", "country",
+                                  "phone_number", "date_of_birth")
+
+            break
+
+    list_filter = UserAdmin.list_filter + ("is_active",)
+
+admin.site.register(User, CustomUserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django_countries.fields import CountryField
+from cities_light.models import City
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+# Extend the User model with a new one
+# that allows more fields
+class User(AbstractUser):
+    # city = City()
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
+
+    country = CountryField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(null=True, blank=True, max_length=10, unique=True)
+
+    # USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS = ["date_of_birth"]
