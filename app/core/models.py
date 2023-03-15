15bb7515df88e8c5c006a070a449c407ba655973
Andreia <andreia@mariadb.org>
2023-03-15 20:36:12 +0200

    Make email the field required for login
    * Email will be required when a user logins
    * Set Username to be required when creating a superuser
        otherwise it gives an error
    * Add the email field in the User model with the
        unique constraint

--- app/core/models.py
+++ app/core/models.py
@@ -7,12 +7,11 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 # Extend the User model with a new one
 # that allows more fields
 class User(AbstractUser):
-    # city = City()
+    email = models.EmailField(max_length=255, unique=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
-
     country = CountryField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     phone_number = models.CharField(null=True, blank=True, max_length=10, unique=True)
 
-    # USERNAME_FIELD = "email"
-    # REQUIRED_FIELDS = ["date_of_birth"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
