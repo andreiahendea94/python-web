486bc7a547e1a5a92f398b4de6b6c6cd2afe2beb
Andreia <andreia@mariadb.org>
2023-03-15 23:19:35 +0200

    Add validation to phone_number and make non-unique

--- app/core/models.py
+++ app/core/models.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import AbstractUser
 from django_countries.fields import CountryField
 from cities_light.models import City
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.core.validators import RegexValidator
 
 # Extend the User model with a new one
 # that allows more fields
 class User(AbstractUser):
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+
     email = models.EmailField(max_length=255, unique=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
     country = CountryField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(null=True, blank=True, max_length=10, unique=True)
+    phone_number = models.CharField(validators=[phone_regex], null=True, blank=True, max_length=17)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
