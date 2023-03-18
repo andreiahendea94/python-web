fb8b7d09d455cc0a1a0d4d6b1c53c59d5bfcd4cf
Andreia <andreia@mariadb.org>
2023-03-18 04:58:36 +0200

    Add DjangoRestFramework and django-cors-headers
    Django Rest Framework communicates with React app

--- app/backend/python_web_test/urls.py
+++ app/backend/python_web_test/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 from . import views
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('', views.index, name='index')
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include(router.urls)),
 ]
