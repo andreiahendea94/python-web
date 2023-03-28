d093206b7b7c06572acdd946a1f7b9990081979e
Andreia <andreia@mariadb.org>
2023-03-28 23:16:17 +0300

    Add endpoint for User functionality
    * Add a serializer for the User model
    * Add ability to display all users
    * Add route for interaction with the User model

--- app/backend/core/views.py
+++ app/backend/core/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
 
 def index(request):
     return HttpResponse("Hello, world. You're at the core index.")
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+    def create(self, request):
+        pass
--- app/backend/python_web_test/urls.py
+++ app/backend/python_web_test/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from core import views
 
-from . import views
+# import views
 router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
-    path('', views.index, name='index')
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include(router.urls)),
 ]
