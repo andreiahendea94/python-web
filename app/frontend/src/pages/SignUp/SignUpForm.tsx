8ed7ecf108319b2428c43fc37b2458c4f88d6273
Andreia <andreia@mariadb.org>
2023-04-04 17:28:05 +0300

    Implement SignUpForm
    Add proper grids with React bootstrap
    Make the Form mobile responsive
    Add new fields and remove the incorrect ones

--- app/frontend/src/pages/SignUp/SignUpForm.tsx
+++ app/frontend/src/pages/SignUp/SignUpForm.tsx
@@ -4,65 +4,71 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Select from 'react-select/async';
+import '../../assets/SignUpForm.css';
+import SimpleSelect from '../../components/Select';
+import DarkGreenButton from '../../components/DarkGreenButton';
 
-export default function RegisterForm() {
+export default function SignUpForm({className}: {className: string}): JSX.Element {
 
   return (
-    <>
-      <Form>
-        <Container fluid className="p-0">
-          <Row className="mb-2">
-            <Form.Group className="sm" as={Col} controlId="formFirstName">
-              <Form.Label column="sm" lg={12}>First Name</Form.Label>
-              <Form.Control type="text" size="sm" placeholder="First name" />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formLastName">
-              <Form.Label column="sm" lg={12}>Last Name</Form.Label>
-              <Form.Control type="text" size="sm" placeholder="Last name" />
-            </Form.Group>
-          </Row>
+    <Form className={className}>
+      <Container fluid className="p-0 SignUpForm__FieldsContainer px-5 py-3">
+        <p className="SignUpForm__Paragraph">Let's do this!</p>
 
-          <Row className="mb-2">
-            <Form.Group className="sm" as={Col} controlId="formEmail">
-              <Form.Label column="sm" lg={12}>Email</Form.Label>
-              <Form.Control type="email" size="sm" placeholder="Email" />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formPassword">
-              <Form.Label column="sm" lg={12}>Password</Form.Label>
-              <Form.Control type="password" size="sm" placeholder="Password" />
-            </Form.Group>
-          </Row>
+        <Row className="mb-2">
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>First Name</Form.Label>
+            <Form.Control type="text" size="sm" placeholder="First name" />
+          </Col>
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Last Name</Form.Label>
+            <Form.Control type="text" size="sm" placeholder="Last name" />
+          </Col>
 
-          <Row className="mb-2">
-            <Form.Group className="sm" as={Col} controlId="formCity">
-              <Form.Label column="sm" lg={12}>City</Form.Label>
-              <Select
-                className="basic-single"
-                classNamePrefix="select"
-                isLoading={true}
-                isSearchable={true}
-                name="city"
-                options={[]}
-              />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formCountry">
-              <Form.Label column="sm" lg={12}>Country</Form.Label>
-              <Select
-                className="basic-single"
-                classNamePrefix="select"
-                isLoading={true}
-                isSearchable={true}
-                name="country"
-                options={[]}
-              />
-            </Form.Group>
-          </Row>
+        </Row>
 
-          <Button variant="primary" type="submit">Submit</Button>
-        </Container>
-      </Form>
-    </>
+        <Row className="mb-2">
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Email</Form.Label>
+            <Form.Control type="email" size="sm" placeholder="Email" />
+          </Col>
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Password</Form.Label>
+            <Form.Control type="password" size="sm" placeholder="Password" />
+          </Col>
+        </Row>
+
+        <Row className="mb-2">
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Date of birth</Form.Label>
+            <Form.Control type="text" size="sm" placeholder="Date of birth" />
+          </Col>
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Phone number</Form.Label>
+            <Form.Control type="text" size="sm" placeholder="Phone number" />
+          </Col>
+        </Row>
+
+        <Row className="mb-2">
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>City</Form.Label>
+            <SimpleSelect />
+          </Col>
+          <Col md={6}>
+            <Form.Label column="sm" lg={12}>Country</Form.Label>
+            <SimpleSelect />
+          </Col>
+        </Row>
+
+      </Container>
+      <Container fluid className="SignUpForm__ButtonContainer">
+        <Row>
+          <Col className="px-5 py-4">
+            <DarkGreenButton type="submit">Create account</DarkGreenButton>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 
 };
