a310d4dd63e8e7808262155324f31b57b2d249d1
Andreia <andreia@mariadb.org>
2023-04-24 22:51:53 +0300

    Add pagination for REST framework

--- app/backend/python_web_test/settings.py
+++ app/backend/python_web_test/settings.py
@@ -137,3 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
